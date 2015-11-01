720ea7ba5bbe7e5b3c3915c3382f8d293a339f34
Chris Sprague <christopherssprague@gmail.com>
2015-11-01 15:32:28 -0500

    Change The Programmed To Be One Line!!!!!

--- jaden.rb
+++ jaden.rb
@@ -1,10 +1,2 @@
 #!/usr/bin/ruby
-
-prefix_fp = File.open("prefixes.txt")
-postfix_fp = File.open("postfixes.txt")
-
-prefix_contents = prefix_fp.read.split("\n")
-postfix_contents = postfix_fp.read.split("\n")
-
-print(prefix_contents.sample + " " + postfix_contents.sample + "\n")
-
+print(File.open("prefixes.txt").read.split("\n").sample+" "+File.open("postfixes.txt").read.split("\n").sample+"\n")
