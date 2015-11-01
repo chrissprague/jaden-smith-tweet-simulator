c72fcb8f0a2d0d6003d55866c415375e5f5ec100
Chris Sprague <christopherssprague@gmail.com>
2015-11-01 15:12:08 -0500

    Implement Postfix And Prefix Joining. No Problem.

--- jaden.rb
+++ jaden.rb
@@ -1,2 +1,10 @@
 #!/usr/bin/ruby
 
+prefix_fp = File.open("prefixes.txt")
+postfix_fp = File.open("postfixes.txt")
+
+prefix_contents = prefix_fp.read.split("\n")
+postfix_contents = postfix_fp.read.split("\n")
+
+print(prefix_contents.sample + " " + postfix_contents.sample + "\n")
+
